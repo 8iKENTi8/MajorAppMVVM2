45bd5fb4de87c8547fb314f024669b13c18b43a6
Vladimir <Vladimir@DESKTOP-37JKIK3>
2024-07-14 16:48:02 +0300

    удаление лишних файлов и корректировки

--- MajorApp/ViewModels/EditOrderViewModel.cs
+++ MajorApp/ViewModels/EditOrderViewModel.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
--- MajorApp/ViewModels/MainViewModel.cs
+++ MajorApp/ViewModels/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
